9562963cce5387f43ba07082a87d637835db6eba
wuzhicong <wuzhicong@guokang.com>
2024-08-21 08:50:25 +0800

    feat: test模块上传

---
+++ dgp-test/src/main/java/com/dgp/test/DgpTestApplication.java
@@ -0,0 +1,13 @@
+package com.dgp.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgpTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DgpTestApplication.class, args);
+    }
+
+}
