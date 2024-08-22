1d5e60250f8ae497b23ece74725097de034af87c
wuzhicong <wuzhicong@guokang.com>
2024-08-22 10:18:15 +0800

    feat: dgp-mybatis提交

---
+++ dgp-mybatis/src/main/java/com/dgp/mybatis/config/MybatisAutoConfiguration.java
@@ -0,0 +1,11 @@
+package com.dgp.mybatis.config;
+
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@ImportAutoConfiguration(MybatisPlusConfig.class)
+@Configuration
+public class MybatisAutoConfiguration {
+
+
+}
--- dgp-test/src/main/java/com/dgp/test/application/TestAppService.java
+++ dgp-test/src/main/java/com/dgp/test/application/TestAppService.java
@@ -1,13 +1,30 @@
 package com.dgp.test.application;
 
+import cn.hutool.crypto.digest.MD5;
+import com.dgp.common.security.md5.Md5EncryptionUtils;
 import com.dgp.core.web.application.BaseAppService;
 import com.dgp.test.entity.Test;
 import com.dgp.test.service.TestService;
 import org.springframework.stereotype.Service;
 
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 @Service
 public class TestAppService extends BaseAppService<TestService, Test> {
     public boolean add(Test test) {
         return bizService.add(test);
+    }
+
+    public static void main(String[] args) {
+        Supplier s = () -> {
+            return "ssss";
+        };
+        System.out.println(s.get());
+        Function<Integer, Integer> f = i -> {
+            return 2 * i;
+        };
+        System.out.println(f.andThen(f).apply(10));
+        System.out.println(Md5EncryptionUtils.getMd5("123456"));
     }
 }
